dc5d17633d0ccfa3495e327df078c1ce46b702e5
JustaNewbee <844266359@qq.com>
2018-05-07 22:45:54 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <TodoList/>
+    </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList'
+import './js/_demo.js'
+export default {
+    name: 'App',
+    components: {
+        TodoList
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+    <input type="text" v-model="inputValue">
+    <button type="button" name="button" @click="handleSubmit">Submit</button>
+    <ul>
+        <li v-for="(item, index) of list" :key="index"  :index="index" @click="handleDelete(index)">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            inputValue: '',
+            list: []
+        }
+    },
+    methods: {
+        handleSubmit () {
+            this.list.push(this.inputValue)
+            this.inputValue = ''
+        },
+        handleDelete (index) {
+            this.list.splice(index, 1)
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
